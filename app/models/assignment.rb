145354f506c19f98cae01c411de3e5c415310296
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-06-11 08:15:29 -0700

    Added association to Assignment and Task

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,5 @@
 class Assignment < ApplicationRecord
-  has_many :tasks 
+  has_many :tasks
+
+  validates_presence_of :title 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :assignment
 
+  validates_presence_of :description
+
   def completed?
     !completed_at.blank?
   end
 
-end#class 
+end#class
