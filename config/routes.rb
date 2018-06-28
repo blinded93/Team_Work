2bb4d154affd3c09655a9d853a7e9f0e3bf32db6
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-06-28 10:18:14 -0700

    Corrected nested route for user and assignments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
- put 'completed_assignment', to: 'assignments#completed', as: :completed_assignment
+  put 'completed_assignment', to: 'assignments#completed', as: :completed_assignment
+
+  resources :users do
+    resources :assignments
+  end 
+
   resources :assignments do
    resources :tasks do
      member do
